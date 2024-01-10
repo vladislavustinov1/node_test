fb7dd90c8c1ab744e484942e66a96dee2ac20f11
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2024-01-10 21:32:39 +0300

    Delete data directory

--- data/config.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-const config = {
-  host: `localhost`,
-  user: `root`,
-  password: `Ou08194321003456123`,
-  database: `api`,
-};
-const pool = mysql.createPool(config);
-module.exports = pool;
