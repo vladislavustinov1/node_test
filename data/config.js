ab28d3ba98a957b57de250259d0a67d2d755d67f
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2024-01-12 23:04:11 +0300

    done

--- data/config.js
+++ data/config.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const config = {
   host: `localhost`,
   user: `root`,
-  password: ``,
+  password: `Ou08194321003456123`,
   database: `api`,
 };
 const pool = mysql.createPool(config);
--- public/frontend.js
+++ public/frontend.js
@@ -1,3 +1,24 @@
+const optionsForButtons = {
+  create: function () {
+    const rules = document.createElement("div");
+    rules.innerHTML = `<h2>Вы админ. Выберите действие</h2>
+    <button>Изменить</button>
+    <button>Добавить</button>`;
+    return rules;
+  },
+  edit: (function () {
+    const rules = document.createElement("div");
+    rules.setAttribute("class", `moderButtons`);
+    rules.innerHTML = `<h2>Вы модер. Выберите действие</h2>
+    <button>Изменить</button>`;
+    return rules;
+  })(),
+  read: (function () {
+    const rules = document.createElement("div");
+    rules.innerHTML = "<h2>Вы обычный юзер. Читайте наши посты!</h2>";
+    return rules;
+  })(),
+};
 let test = document.getElementById("container");
 fetch("http://localhost:3000/posts")
   .then((response) => response.json())
@@ -13,24 +34,10 @@ fetch("http://localhost:3000/posts")
     fetch("http://localhost:3000/user")
       .then((response) => response.json())
       .then((data2) => {
-        console.log(data2);
-        const div2 = document.getElementsByClassName("posts");
+        const div2 = document.querySelectorAll(".posts");
+        const buttons = optionsForButtons[data2];
         for (let i = 0; i < div2.length; i++) {
-          const rules = document.createElement("div");
-          if (data2 == "create") {
-            rules.innerHTML = `<h2>Вы админ. Выберите действие</h2>
-                              <button>Изменить</button>
-                              <button>Добавить</button>`;
-          } else if (data2 == "edit") {
-            rules.innerHTML = `<h2>Вы модер. Выберите действие</h2>
-                              <button>Изменить</button>`;
-          } else {
-            rules.innerHTML = "<h2>Вы обычный юзер. Читайте наши посты!</h2>";
-            div2[i].appendChild(rules);
-            break;
-          }
-          div2[i].appendChild(rules);
+          div2[i].appendChild(buttons());
         }
-        console.log(data2);
       });
   });
