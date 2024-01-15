7db44a6c2a8ca47d1b3fed34d5b8be387d877ff4
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2024-01-15 14:28:59 +0300

    edit frontend.js(sorting JSON)
    return higher permissions for edit content in page(using buttons depending on the user permissions)

--- public/frontend.js
+++ public/frontend.js
@@ -1,29 +1,28 @@
 const optionsForButtons = {
   create: function () {
     const rules = document.createElement("div");
-    rules.innerHTML = `<h2>Вы админ. Выберите действие</h2>
+    rules.innerHTML = `<h2>Выберите действие</h2>
     <button>Изменить</button>
     <button>Добавить</button>`;
     return rules;
   },
-  edit: (function () {
+  edit: function () {
     const rules = document.createElement("div");
     rules.setAttribute("class", `moderButtons`);
-    rules.innerHTML = `<h2>Вы модер. Выберите действие</h2>
+    rules.innerHTML = `<h2>Выберите действие</h2>
     <button>Изменить</button>`;
     return rules;
-  }),
-  read: (function () {
+  },
+  read: function () {
     const rules = document.createElement("div");
     rules.innerHTML = "<h2>Вы обычный юзер. Читайте наши посты!</h2>";
     return rules;
-  }),
+  },
 };
 let test = document.getElementById("container");
 fetch("http://localhost:3000/posts")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     data.forEach((item) => {
       const div = document.createElement("div");
       div.setAttribute("class", "posts");
@@ -34,8 +33,14 @@ fetch("http://localhost:3000/posts")
     fetch("http://localhost:3000/user")
       .then((response) => response.json())
       .then((data2) => {
+        let getPermission = data2.split(", ").sort();
+        if (getPermission.length > 1) {
+          getPermission.pop();
+        }
+        getPermission = getPermission.join("");
         const div2 = document.querySelectorAll(".posts");
-        const buttons = optionsForButtons[data2];
+        const buttons = optionsForButtons[getPermission];
+        console.log(buttons);
         for (let i = 0; i < div2.length; i++) {
           div2[i].appendChild(buttons());
         }
