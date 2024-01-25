58aedf613ee29703198aa825f017765353bb8a98
Влад Устинов <98713367+vladislavustinov1@users.noreply.github.com>
2024-01-25 12:42:44 +0300

    update code

--- public/frontend.js
+++ public/frontend.js
@@ -1,48 +1,59 @@
 const optionsForButtons = {
-  create: function () {
+  create() {
     const rules = document.createElement("div");
     rules.innerHTML = `<h2>Выберите действие</h2>
     <button>Изменить</button>
     <button>Добавить</button>`;
     return rules;
   },
-  edit: function () {
+  edit() {
     const rules = document.createElement("div");
     rules.setAttribute("class", `moderButtons`);
     rules.innerHTML = `<h2>Выберите действие</h2>
     <button>Изменить</button>`;
     return rules;
   },
-  read: function () {
+  read() {
     const rules = document.createElement("div");
     rules.innerHTML = "<h2>Вы обычный юзер. Читайте наши посты!</h2>";
     return rules;
   },
 };
-let test = document.getElementById("container");
-fetch("http://localhost:3000/posts")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((item) => {
-      const div = document.createElement("div");
-      div.setAttribute("class", "posts");
-      div.innerHTML = `<h2>${item.title}</h2>
+const API_POSTS = "http://localhost:3000/posts";
+const API_USERS = "http://localhost:3000/user";
+const test = document.getElementById("container");
+Promise.all([
+  fetch(API_POSTS).then((response) => response.json()),
+  fetch("http://localhost:3000/user").then((response) => response.json()),
+])
+  .then(([postAllData, userPermission]) => {
+    postAllData.forEach((item) => {
+      const wrapperForPosts = document.createElement("div");
+      wrapperForPosts.setAttribute("class", "posts");
+      wrapperForPosts.innerHTML = `<h2>${item.title}</h2>
                         <p>${item.text}</p>`;
-      test.appendChild(div);
+      test.appendChild(wrapperForPosts);
     });
-    fetch("http://localhost:3000/user")
-      .then((response) => response.json())
-      .then((data2) => {
-        let getPermission = data2.split(", ").sort();
-        if (getPermission.length > 1) {
-          getPermission.pop();
-        }
-        getPermission = getPermission.join("");
-        const div2 = document.querySelectorAll(".posts");
-        const buttons = optionsForButtons[getPermission];
-        console.log(buttons);
-        for (let i = 0; i < div2.length; i++) {
-          div2[i].appendChild(buttons());
-        }
-      });
+    console.log(userPermission);
+    let getPermission = userPermission.split(", ").sort();
+    const MIN_SIZE_PERMISSIONS_FOR_DELETE = 1;
+    if (getPermission.length > MIN_SIZE_PERMISSIONS_FOR_DELETE) {
+      for (let countRole = 0; countRole < getPermission.length; countRole++) {
+        getPermission.pop();
+      }
+    }
+    getPermission = getPermission.join("");
+
+    const wrapperForButtons = document.querySelectorAll(".posts");
+    const buttons = optionsForButtons[getPermission];
+    for (
+      let numberOfPosts = 0;
+      numberOfPosts < wrapperForButtons.length;
+      numberOfPosts++
+    ) {
+      wrapperForButtons[numberOfPosts].appendChild(buttons());
+    }
+  })
+  .catch((error) => {
+    console.error(error);
   });
